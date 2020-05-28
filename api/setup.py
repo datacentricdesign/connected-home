a10be08fd18185c3560e11eb9ac8e9102c51420a
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-05-28 08:04:28 +0200

    Bumped version number to 0.0.6

--- api/setup.py
+++ api/setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name="connected-home",
-    version="0.0.5",
+    version="0.0.6",
     description="A teaching environment simulating a connected home.",
     license="MIT",
     author="Data-Centric Design Lab <lab@datacentricdesign.org>",
