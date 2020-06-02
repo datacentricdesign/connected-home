e28dcdd7fd246d838135fdfa7cae536b3290a4c9
livein21st <niravpmalsattar@gmail.com>
2020-06-02 13:44:39 +0200

    Updating the ReadMe and development instruction in sperate file

--- api/connected-home/__main__.py
+++ api/connected-home/__main__.py
@@ -25,40 +25,49 @@ def findThingById(thing_id):
         if thing.id == thing_id:
             return thing
 
+
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@app.route('/things', methods = ['GET'])
+
+@app.route('/things', methods=['GET'])
 def list():
     return json.dumps([thing.__dict__ for thing in things])
 
-@app.route('/things/<path:thing_id>', methods = ['GET'])
+
+@app.route('/things/<path:thing_id>', methods=['GET'])
 def read(thing_id):
     global things
     return json.dumps(findThingById(thing_id).__dict__)
 
-@app.route('/things/<path:thing_id>/controls/<path:control_id>', methods = ['GET'])
+
+@app.route('/things/<path:thing_id>/controls/<path:control_id>', methods=['GET'])
 def control(thing_id, control_id):
     print('control ' + control_id + ' of ' + thing_id)
     response = {}
     response["result"] = getattr(findThingById(thing_id), control_id)()
     return json.dumps(response)
 
-@app.route('/things', methods = ['POST'])
+
+@app.route('/things', methods=['POST'])
 def create():
     global things
-    module = __import__("connected-home.entities." + request.json["type"].lower(), fromlist=['object'])
+    module = __import__("connected-home.entities." +
+                        request.json["type"].lower(), fromlist=['object'])
     ThingClass = getattr(module, request.json["type"])
     things.append(ThingClass(request.json["name"]))
     return 'Added thing of type ' + request.json["type"] + '!'
+
 
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
     emit('json', json, broadcast=True)
 
+
 def send_ws_message(json):
     emit('json', json, broadcast=True)
+
 
 light1 = Light('Test light')
 switch1 = Switch('Test switch')
@@ -68,5 +77,3 @@ things = [light1, switch1]
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=80)
-    
-
